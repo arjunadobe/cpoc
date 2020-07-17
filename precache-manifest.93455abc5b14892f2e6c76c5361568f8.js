2b42a82f8c3d85b6533876501dbcc49cb35b4df4
Arjun Adobe <51747982+arjunadobe@users.noreply.github.com>
2020-07-18 03:02:39 +0530

    Updates

---
+++ precache-manifest.93455abc5b14892f2e6c76c5361568f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bde77cfea991a19ce24dcba8da0e9e5",
+    "url": "/cpoc/index.html"
+  },
+  {
+    "revision": "bc1b8909c8f610e8b91b",
+    "url": "/cpoc/static/css/4.ab807fd7.chunk.css"
+  },
+  {
+    "revision": "991ced2eceaaa0314e5a",
+    "url": "/cpoc/static/css/main.e7b3c5d1.chunk.css"
+  },
+  {
+    "revision": "89a22aeb0fd5e51a627a",
+    "url": "/cpoc/static/js/2.43cabdd6.chunk.js"
+  },
+  {
+    "revision": "1790214efba34982a154466015e58e1d",
+    "url": "/cpoc/static/js/2.43cabdd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175e722d4ae82d4d2492",
+    "url": "/cpoc/static/js/3.076a0cb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cpoc/static/js/3.076a0cb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1b8909c8f610e8b91b",
+    "url": "/cpoc/static/js/4.cd19fef4.chunk.js"
+  },
+  {
+    "revision": "991ced2eceaaa0314e5a",
+    "url": "/cpoc/static/js/main.b9bab1e8.chunk.js"
+  },
+  {
+    "revision": "ad17c0950c04333eb6c3",
+    "url": "/cpoc/static/js/runtime-main.7fb5c3d4.js"
+  }
+]);
