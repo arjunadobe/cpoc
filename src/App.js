5b45013776ca83dd42a9d7f88049b857edcd935b
Arjun Adobe <51747982+arjunadobe@users.noreply.github.com>
2020-07-22 19:02:29 +0530

    Rectangle and loader

--- src/App.js
+++ src/App.js
@@ -1,61 +1,13 @@
 import React from "react";
-import { Space, Layout, Menu } from "antd";
 import "./App.less";
-import {
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+
 import ScannerPrototype from "./components/scannerPrototype/scannerPrototype";
-const { Header, Content, Footer, Sider } = Layout;
 
 const App = () => {
   return (
-    <div>
-      <Layout style={{ height: "100vh" }}>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <section style={{ textAlign: "center", margin: 16 }}>
-            <Space align="start">
-              <img
-                style={{height: 36}}
-                src="https://www.centralgroup.com/themes/default/assets/static/images/logo/logo-white-gray.svg"
-                alt="Centra Group"
-              />
-            
-            </Space>
-          </section>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-            <Menu.Item key="1" icon={<VideoCameraOutlined />}>
-              Scan
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header
-            className="site-layout-sub-header-background"
-            style={{ padding: 0 }}
-          />
-
-          <Content style={{ margin: "0px 16px 0" }}>
-            <div
-              className="site-layout-background"
-              style={{ padding: 24, minHeight: 360 }}
-            >
-              <ScannerPrototype></ScannerPrototype>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-          </Footer>
-        </Layout>
-      </Layout>
-    </div>
+      <div>
+        <ScannerPrototype></ScannerPrototype>
+      </div>
   );
 };
 
--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,110 +1,100 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from "react";
 import * as zxing from "@zxing/library";
-import { Button } from "antd";
 import "./video.css";
 
 const Video = () => {
-  const [errorMessage, setErrorMessage] = useState("");
-  const [recording, setRecording] = useState(false);
-  const hints = new Map();
-  hints.set(zxing.DecodeHintType.TRY_HARDER, true);
-  const reader = new zxing.BrowserMultiFormatReader(hints, 1000);
+    const [errorMessage, setErrorMessage] = useState("");
+    const hints = new Map();
+    hints.set(zxing.DecodeHintType.TRY_HARDER, true);
+    const reader = new zxing.BrowserMultiFormatReader(hints, 1000);
 
-  const start = (e) => {
-    setRecording(true);
-    const p1 = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reader.stopAsyncDecode();
-      }, 160000 * 1);
-
-      reader
-          .decodeOnceFromVideoDevice(null, document.getElementById(
-              "video"
-          ))
-          .then(resolve)
-          .catch(reject);
-
-
-    })
-        .then((result: zxing.Result) => {
-          alert(result.getText());
-          stopStreamedVideo();
+    const start = () => {
+        const p1 = new Promise((resolve, reject) => {
+            reader.decodeOnceFromVideoDevice(null, document.getElementById(
+                "video"
+            ))
+                .then(resolve)
+                .catch(reject);
         })
-        .catch(err => {
-          console.log(err.message);
-          if (
-              err.message !==
-              "Video stream has ended before any code could be detected."
-          ) {
-            setErrorMessage(
-                "Video stream has ended before any code could be detected."
-            );
-          }
-          stopStreamedVideo();
+            .then((result: zxing.Result) => {
+                document.getElementById("idcanvas").style.display = "none";
+                alert(result.getText());
+                start();
+                var video = document.getElementById('video');
+                video.addEventListener('loadedmetadata', function(e){
+                    document.getElementById("idcanvas").style.display = "block";
+                });
+            })
+            .catch(err => {
+                setErrorMessage(err.message);
+            });
+        p1.catch(error => {
+            setErrorMessage(error.message);
         });
 
-    p1.catch(error => {
-      console.log(error);
+    };
+
+    useEffect(() => {
+        startOnload();
     });
-  };
 
-  const stop = (e) => {
-    e.preventDefault();
-    reader.stopAsyncDecode();
-    stopStreamedVideo();
-  };
-
-  function stopStreamedVideo() {
-    const videoElem: any = document.getElementById("video");
-    setRecording(false);
-
-    if (videoElem && videoElem.srcObject) {
-      let stream = videoElem.srcObject;
-      let tracks = stream.getTracks();
-
-      tracks.forEach((track: MediaStreamTrack) => {
-        track.stop();
-      });
-
-      videoElem.srcObject = null;
+    function startOnload() {
+        start();
     }
-  }
 
-  return (
-      <div className="container-fluid">
-        <div className="w-100 row align-items-center">
-          <div className="row">{errorMessage}</div>
-          <div className="row">
-            {!recording && (
-                <Button
-                    type="primary"
-                    id="btnStart"
-                    htmlType="submit"
-                    onClick={e => start(e)}
-                    style={{ marginTop: 10 }}
-                >
-                  Scan Code
-                </Button>
-            )}
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment'}})
+            .then( function(stream) {
+                var video = document.getElementById('video');
+                video.addEventListener('loadedmetadata', function(e){
+                    drawImage();
+                });
+            }
+        )
+    };
 
-            {recording && (
-                <Button
-                    type="primary"
-                    id="btnStop"
-                    htmlType="submit"
-                    onClick={e => stop(e)}
-                    style={{ marginTop: 10 }}
-                >
-                  Stop scanning
-                </Button>
-            )}
-          </div>
-          <div className="video">
-            <video id="video" />
-          </div>
+    function addClassName() {
+        let classes = "video ";
+        return classes;
+    }
+
+    function drawImage() {
+        var canvas = document.querySelector("#idcanvas");
+        if (canvas !== null) {
+            var ctsx = canvas.getContext('2d');
+            ctsx.beginPath();
+            canvas.width = window.screen.width;
+            canvas.height = window.screen.height;
+            canvas.style.width = "100%";
+            canvas.style.height = "100%";
+            var faceArea = 300;
+            var pX = canvas.width / 2 - faceArea / 2;
+            var pY = canvas.height / 2 - faceArea / 2;
+            ctsx.clearRect(0, 0, canvas.width, canvas.height);
+            ctsx.rect(pX, pY, faceArea, faceArea);
+            ctsx.lineWidth = "6";
+            ctsx.strokeStyle = "#fe8e14";
+            ctsx.stroke();
+        }
+    }
+
+    return (
+        <div className="container-fluid">
+            <div className="dynam-info">
+                <img src="https://www.centralgroup.com/themes/default/assets/static/images/logo/logo-white-gray.svg" alt="logo"/>
+            </div>
+            <div id="loading">
+                <img id="loading-image" src={process.env.PUBLIC_URL + '/bike_loader.gif'} alt="Loading..." />
+            </div>
+            <div className="w-100 row align-items-center">
+                <div className="row">{errorMessage}</div>
+                <div className={addClassName()}>
+                    <video id="video"/>
+                    <canvas id="idcanvas"></canvas>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default Video;
